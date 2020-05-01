d48c28be3eaa01ff4ea1e9138e3c44f731192ded
Mehdi Najafi <mehdinajafi20.mn@gmail.com>
2020-05-01 15:47:28 +0430

    scripts for add product to list

--- scripts/functions.js
+++ scripts/functions.js
@@ -8,3 +8,127 @@ const getProducts = () => {
 }
 
 const saveProducts = (products) => localStorage.setItem("products", JSON.stringify(products))
+
+let showSum = () => {
+    sumPrices = 0
+    products.forEach(element => {
+        sumPrices+=element.price
+    });
+    document.querySelector("#sum").innerHTML = `$${sumPrices}`
+}
+
+let lastUpdated = (time) => {
+    return moment(time).locale('fa').fromNow()
+}
+
+let sortProduct = (products, sortBy) => {
+    return products.sort((a,b) => {
+        if(sortBy === "byUpdated"){
+            if(a.updated > b.updated){
+                return -1
+            } else if(a.updated < b.updated){
+                return 1
+            } else {
+                return 0
+            }
+        } else if(sortBy === "byCreated"){
+            if(a.created> b.created){
+                return -1
+            } else if(a.created < b.created){
+                return 1
+            } else {
+                return 0
+            }
+        } else {
+            return []
+        }
+    })
+}
+
+const createElement = (product) => {
+    let trProduct = document.createElement("tr")
+    trProduct.setAttribute("id", product.id)
+    trProduct.setAttribute("class", "product")
+
+    let tdCheckbox = document.createElement("td")
+    let inputCheckbox = document.createElement("input")
+    let labelCheckbox = document.createElement("label")
+    let tdTitle = document.createElement("td")
+    let aTitle = document.createElement("a")
+    let tdPrice = document.createElement("td")
+    let tdCreated = document.createElement("td")
+    let tdUpdated = document.createElement("td")
+    let tdRemoveButton = document.createElement("td")
+    let removeButton = document.createElement("button")
+
+    // ckeckBox
+    labelCheckbox.textContent = "ناموجود"
+    inputCheckbox.setAttribute("type", "checkbox")
+    inputCheckbox.setAttribute("id", product.id.slice(0, 4))
+    labelCheckbox.setAttribute("for", product.id.slice(0, 4))
+    inputCheckbox.addEventListener("change", (e) => {
+        if(e.target.checked){
+            product.exist = false
+            e.target.checked = "0"
+        } else {
+            product.exist = true
+        }
+        saveProducts(products)
+    })
+    tdCheckbox.appendChild(inputCheckbox)
+    tdCheckbox.appendChild(labelCheckbox)
+
+    // Title
+    aTitle.textContent = product.title
+    aTitle.setAttribute("href", `./edit-product.html#${product.id}`)
+    tdTitle.appendChild(aTitle)
+
+    // Price
+    tdPrice.textContent = `$${product.price}`
+    // Created
+    tdCreated.textContent = moment(product.created).locale('fa').fromNow()
+    
+    // Updated 
+    tdUpdated.textContent = lastUpdated(product.updated)
+
+    // remove button
+    removeButton.textContent = "حذف محصول"
+    removeButton.setAttribute("id", "removeButton")
+    removeButton.addEventListener("click", () => {
+        removeProduct(product.id)
+        saveProducts(products)
+        renderProduct(products,filters)
+    })
+    tdRemoveButton.appendChild(removeButton)
+
+    trProduct.appendChild(tdCheckbox)
+    trProduct.appendChild(tdTitle)
+    trProduct.appendChild(tdPrice)
+    trProduct.appendChild(tdCreated)
+    trProduct.appendChild(tdUpdated)
+    trProduct.appendChild(tdRemoveButton)
+    return trProduct
+}
+
+
+const renderProduct = (products, filters) => {
+    sortProduct(products, filters.sortBy)
+    let filtered = products.filter((item)=> {
+        return item.title.toLowerCase().includes(filters.searchItems.toLowerCase())
+    })
+    let a = document.querySelectorAll(".product")
+    a.forEach(item => {
+        item.remove()
+    })
+    filtered = filtered.filter((item) => {
+        if(filters.availableProduct){
+            return item.exist
+        } else {
+            return true
+        }
+    })
+    filtered.forEach(item => {
+        document.querySelector("tbody").appendChild(createElement(item))
+    })
+    showSum()
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,25 @@
 let products = getProducts()
+
+let filters = {
+    searchItems: ''
+}
+
+let sumPrices = 0
+showSum()
+renderProduct(products, filters)
+document.querySelector("#addProduct").addEventListener("submit", (e) => {
+    e.preventDefault()
+    let time = moment().valueOf()
+    products.push({
+        id: uuidv4(),
+        title: e.target.elements.productTitle.value,
+        price: Number(e.target.elements.productPrice.value),
+        exist: true,
+        created: time,
+        updated: time
+    })
+    renderProduct(products, filters)
+    saveProducts(products)
+    e.target.elements.productTitle.value = ''
+    e.target.elements.productPrice.value = ''
+})
